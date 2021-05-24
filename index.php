8bb0de1b3c67c7b2feef30a5a737b234e9030b2e
JohannL <github@johann-lau.de>
2021-05-24 04:37:30 +0200

    whoops

--- index.php
+++ index.php
@@ -7,10 +7,4 @@ define('OC_HOSTNAME', $_SERVER['HTTP_HOST']);
 define('OC_PATH', dirname($_SERVER['SCRIPT_NAME']) . '/');
 define('OC_GET', substr($_SERVER['REQUEST_URI'], strlen(OC_PATH)));
 
-define('DB_HOST',			'127.0.0.1');
-define('DB_USERNAME',		'root');
-define('DB_PASSWORD',		'');
-define('DB_DB_NAME',		'omnichat');
-define('DB_TABLE_PREFIX',	'');
-
 $OC = new OC();
